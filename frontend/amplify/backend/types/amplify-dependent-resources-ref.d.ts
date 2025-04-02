3dd1358b5f9331150d5a3a6a73a95549898a7e09
KoushikReddyB <2200090018@kluniversity.in>
2025-04-02 12:50:35 +0530

    Deployments to Amplify - ERRORs needs to be fixed

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,34 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apic64d4410": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "LoginLambdaFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "RegisterVerifyLambdaFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "registerInitLambdaFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "hosting": {
     "S3AndCloudFront": {
       "CloudFrontDistributionID": "string",
